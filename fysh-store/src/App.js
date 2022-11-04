0671f62a7d0ccd5cf0d4ef1ec1845e6b91540699
daitkenbacon <104740593+daitkenbacon@users.noreply.github.com>
2022-11-04 18:10:06 -0500

    updating directory component and category comonent

---
+++ fysh-store/src/App.js
@@ -0,0 +1,11 @@
+import Directory from './components/directory/directory.component';
+import categories from './categories.json';
+
+const App = () => {
+
+  return (
+    <Directory categories={categories} />
+  );
+}
+
+export default App;

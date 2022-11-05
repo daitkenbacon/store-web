f33ec23f0f2e5d0e625a84e20d9556871a1220cb
daitkenbacon <104740593+daitkenbacon@users.noreply.github.com>
2022-11-05 11:10:35 -0500

    Created authentication page component with sign-in-form

--- fysh-store/src/App.js
+++ fysh-store/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
 import NavBar from './routes/navigation/navbar.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 
 const App = () => {
 
@@ -10,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<NavBar />} >
         <Route index element={<Home />}/>
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
